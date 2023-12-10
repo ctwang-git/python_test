99198410d8ec22694892c0b8665ad9450959099b
ctwang-git <62320657+ctwang-git@users.noreply.github.com>
2023-12-11 02:47:53 +0800

    modify Line function : add LineItem class

--- PR_Open_AI.py
+++ PR_Open_AI.py
@@ -1,15 +1,19 @@
 
 
 import sys, time, os
+from PyQt5.QtWidgets import QGraphicsSceneMouseEvent
 import numpy as np
 import cv2
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from Ui_main import Ui_MainWindow
-
-class webcam(QtCore.QThread):   
-    rawdata = QtCore.pyqtSignal(np.ndarray) 
+import math
+class webcam(QThread):   
+    rawdata = pyqtSignal(np.ndarray) 
     def __init__(self, parent=None):
-        rawdata = QtCore.pyqtSignal(np.ndarray)
+        rawdata = pyqtSignal(np.ndarray)
         super().__init__(parent)
         
         self.camera = cv2.VideoCapture(0)
@@ -66,30 +70,113 @@ class webcam(QtCore.QThread):
     def getmode(self):
         return self.mode
 
+# 
+def content_pos(x1, y1, x2, y2):
+    deltaX = x2 - x1
+    deltaY = y2 - y1
+    dis = math.sqrt(deltaX **2 + deltaY **2)
+    if deltaX == 0:
+        angle = 0
+    else:
+        angle = math.atan(deltaY / deltaX)
+        angle = math.degrees(angle)
+    minX = (x1 + x2) / 2
+    minY = (y1 + y2) / 2
+    return dis, angle, minX, minY
+ 
+
+class LineItem(QGraphicsLineItem, QGraphicsRectItem, QGraphicsSimpleTextItem):
+    NORM_DRAW, ORTH_DRAW = 0, 1
+    def __init__(self, parent=None):
+        super(LineItem, self).__init__(parent)
+        self.line = None
+        self.text = None
+        self.startRect = None
+        self.endRect = None
+        self.startPt = QPointF()
+        self.endPt = QPointF()
+        self.distance = 0
+        self.angle = 0
+        self.minX = 0
+        self.minY = 0
+        self.mode = self.NORM_DRAW
+    def setLinetoOrth(self):
+        self.mode = self.ORTH_DRAW       
+    def setLineNormal(self):
+        self.mode = self.NORM_DRAW
+    def releaseItem(self):
+        self.line = None
+        self.text = None
+        self.startRect = None
+        self.endRect = None
+    def initItem(self):
+        self.line = QGraphicsLineItem()
+        self.text = QGraphicsTextItem()
+        self.startRect = QGraphicsRectItem()
+        self.endRect = QGraphicsRectItem()
+    def getDrawing(self):
+        return self.line is not None
+    def getItem(self):
+        return self.line, self.text, self.startRect, self.endRect
+    def drawingLine(self, pos):
+        self.endPt = pos
+        if self.mode == self.ORTH_DRAW:
+            deltaX = math.fabs(self.startPt.x() - self.endPt.x())
+            deltaY = math.fabs(self.startPt.y() - self.endPt.y())
+            if deltaX >= deltaY:
+                line = [self.startPt.x(), self.startPt.y(), self.endPt.x(), self.startPt.y()]
+            else:
+                line = [self.startPt.x(), self.startPt.y(), self.startPt.x(), self.endPt.y()]
+        else:    
+            line = [self.startPt.x(), self.startPt.y(), self.endPt.x(), self.endPt.y()]
+        self.line.setLine(line[0], line[1], line[2], line[3])  
+        self.distance, self.angle, self.minX, self.minY = content_pos(line[0], line[1], line[2], line[3])              
+        self.startRect.setRect(self.startPt.x() - 5, self.startPt.y() - 5, 10, 10)
+        self.endRect.setRect(line[2] - 5, line[3] - 5, 10, 10)
+        self.text.setPlainText(f'{self.distance:.1f}')
+        self.text.setPos(self.minX, self.minY)
+        self.text.setRotation(self.angle)
+    def mousePressEvent(self, pos):
+        if self.line is None:
+            self.startPt = pos
+            self.initItem()
+        else:
+            self.drawingLine(pos) 
+            self.releaseItem()
+    def mouseMoveEvent(self, pos):
+        if self.line is not None:
+            self.drawingLine(pos) 
+            
+
+
+
+
+       
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):  
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)        
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)        
         # self.viewData.setScaledContents(True)
         self.viewData.setMouseTracking(True)
-        # self.viewData.mouseMoveEvent = self.mousemoveEvent
-        # self.viewData.mousePressEvent = self.mousepressEvent
-        self.frame = QtWidgets.QGraphicsPixmapItem()        
-        self.scene = QtWidgets.QGraphicsScene()
-        self.scene.addItem(self.frame)
-       
-        self.scene.mouseMoveEvent = self.mousemoveEvent
-        self.scene.mousePressEvent = self.mousepressEvent
         
+        self.frame = QGraphicsPixmapItem()        
+        self.scene = QGraphicsScene()
+        self.scene.addItem(self.frame)
+        self.itemLine = LineItem()
+        self.itemLines = []
+        self.scene.mouseMoveEvent = self.lineMouseMove
+        self.scene.mousePressEvent = self.lineMousePress
+        self.scene.keyPressEvent = self.linekeypress
         self.view_x = self.view.horizontalScrollBar()
         self.view_y = self.view.verticalScrollBar()
-        self.startPt = QtCore.QPointF()
-        self.endPt = QtCore.QPointF()
-        self.curline = None
-        self.scopline = None
-        self.drawStep = 0
-        self.vector = np.array([])
+        
+        # self.startPt = QtCore.QPointF()
+        # self.endPt = QtCore.QPointF()
+        # self.curline = None
+        # self.scopline = None
+        # self.drawStep = 0
+        # self.vector = np.array([])
         
         self.view.installEventFilter(self)
         self.last_move_x = 0
@@ -107,7 +194,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.btnCamStop.clicked.connect(self.stopCam)
         self.btnSave.clicked.connect(self.saveCam)
         self.btnCamAI.clicked.connect(self.ai)
-        # self.btnLine.clicked.connect(self.line)
+        self.btnLine.clicked.connect(self.line)
         
         self.btnCamStop.setEnabled(False)
         self.btnCamAI.setEnabled(False)
@@ -134,62 +221,77 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.cbViewROI.setEnabled(False)
     def ai(self):
         self.useCam.setmode(1)
-    # def line(self):
-    #     self.useCam.setmode(0)
     def saveCam(self):
         self.useCam.saveImg(self.txtLot.text())
-    def mousemoveEvent(self, event):
-        if self.scopline is not None:
-            if self.drawStep == 0:
-                self.endPt = event.scenePos()
-                X = self.startPt.x() * 2 - self.endPt.x()
-                Y = self.startPt.y() * 2 - self.endPt.y()
-                self.vector = np.array([self.endPt.y() - self.startPt.y(), -self.endPt.x() + self.startPt.x()])
-                self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + self.vector[0], self.startPt.y() + self.vector[1])
-                self.scopline.setLine(X, Y, self.endPt.x(), self.endPt.y())
-            else:
-                self.endPt = event.scenePos()
-                w = np.array([self.endPt.x() - self.startPt.x(), self.endPt.y() - self.startPt.y()])
-                mu = np.dot(w, self.vector) / np.dot(self.vector, self.vector)
-                # mu = np.clip(mu, 0, 1)
-                dv = mu * self.vector 
-                self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + dv[0], self.startPt.y() + dv[1])
-    def mousepressEvent(self, event):
-        if self.scopline is None:
-            self.startPt = event.scenePos()
-            self.scopline = QtWidgets.QGraphicsLineItem(self.startPt.x(), self.startPt.y(), self.startPt.x(), self.startPt.y())
-            self.curline = QtWidgets.QGraphicsLineItem(self.startPt.x(), self.startPt.y(), self.startPt.x(), self.startPt.y())
-            self.scopline.setPen(QtCore.Qt.yellow)
-            pen = QtGui.QPen(QtCore.Qt.yellow, 1, QtCore.Qt.DotLine)
-            self.curline.setPen(pen)            
-            self.scene.addItem(self.scopline)
-            self.scene.addItem(self.curline)
-        else:
-            self.endPt = event.scenePos()
-            if self.drawStep == 0:                
-                X = self.startPt.x() * 2 - self.endPt.x()
-                Y = self.startPt.y() * 2 - self.endPt.y()
-                self.scopline.setLine(X, Y, self.endPt.x(), self.endPt.y())
-                self.vector = np.array([self.endPt.y() - self.startPt.y(), -self.endPt.x() + self.startPt.x()])
-                self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + self.vector[0], self.startPt.y() + self.vector[1])
-                # self.scopline = None
-                self.drawStep = 1
-            else:
-                w = np.array([self.endPt.x() - self.startPt.x(), self.endPt.y() - self.startPt.y()])
-                mu = np.dot(w, self.vector) / np.dot(self.vector, self.vector)
-                # mu = np.clip(mu, 0, 1)
-                dv = mu * self.vector 
-                self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + dv[0], self.startPt.y() + dv[1])
-                self.curline.setPen(QtCore.Qt.yellow)
-                radius = (dv[0]**2 + dv[1] **2) ** 0.5
-                center_x, center_y = self.startPt.x() + dv[0], self.startPt.y() + dv[1]
-                circle = QtWidgets.QGraphicsEllipseItem(center_x - radius, center_y - radius, 2 * radius, 2 * radius)
-                pen = QtGui.QPen(QtCore.Qt.yellow, 1, QtCore.Qt.DotLine)
-                circle.setPen(pen)            
-                self.scene.addItem(circle)
-                self.curline = None
-                self.scopline = None
-                self.drawStep = 0
+    def line(self):
+        self.useCam.setmode(0)
+        print('btnLine is clicked')
+        self.scene.mouseMoveEvent = self.lineMouseMove
+        self.scene.mousePressEvent = self.lineMousePress
+    def lineMouseMove(self, event):
+        self.itemLine.mouseMoveEvent(event.scenePos())    
+        # if self.scopline is not None:
+        #     if self.drawStep == 0:
+        #         self.endPt = event.scenePos()
+        #         X = self.startPt.x() * 2 - self.endPt.x()
+        #         Y = self.startPt.y() * 2 - self.endPt.y()
+        #         self.vector = np.array([self.endPt.y() - self.startPt.y(), -self.endPt.x() + self.startPt.x()])
+        #         self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + self.vector[0], self.startPt.y() + self.vector[1])
+        #         self.scopline.setLine(X, Y, self.endPt.x(), self.endPt.y())
+        #     else:
+        #         self.endPt = event.scenePos()
+        #         w = np.array([self.endPt.x() - self.startPt.x(), self.endPt.y() - self.startPt.y()])
+        #         mu = np.dot(w, self.vector) / np.dot(self.vector, self.vector)
+        #         # mu = np.clip(mu, 0, 1)
+        #         dv = mu * self.vector 
+        #         self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + dv[0], self.startPt.y() + dv[1])
+    def linekeypress(self, event):
+        if event.key() == Qt.Key_O:
+            self.itemLine.setLinetoOrth()
+        elif event.key() == Qt.Key_N:
+             self.itemLine.setLineNormal()   
+    def lineMousePress(self, event):
+        self.itemLine.mousePressEvent(event.scenePos())
+        if self.itemLine.getDrawing():
+            for item in self.itemLine.getItem():
+                self.scene.addItem(item)
+        # else:
+        #     self.scene.removeItem(self.itemLine.getItem())
+        # if self.scopline is None:
+        #     self.startPt = event.scenePos()
+        #     self.scopline = QGraphicsLineItem(self.startPt.x(), self.startPt.y(), self.startPt.x(), self.startPt.y())
+        #     self.curline = QGraphicsLineItem(self.startPt.x(), self.startPt.y(), self.startPt.x(), self.startPt.y())
+        #     self.scopline.setPen(QtCore.Qt.yellow)
+        #     pen = QPen(QtCore.Qt.yellow, 1, QtCore.Qt.DotLine)
+        #     self.curline.setPen(pen)            
+        #     self.scene.addItem(self.scopline)
+        #     self.scene.addItem(self.curline)
+        # else:
+        #     self.endPt = event.scenePos()
+        #     if self.drawStep == 0:                
+        #         X = self.startPt.x() * 2 - self.endPt.x()
+        #         Y = self.startPt.y() * 2 - self.endPt.y()
+        #         self.scopline.setLine(X, Y, self.endPt.x(), self.endPt.y())
+        #         self.vector = np.array([self.endPt.y() - self.startPt.y(), -self.endPt.x() + self.startPt.x()])
+        #         self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + self.vector[0], self.startPt.y() + self.vector[1])
+        #         # self.scopline = None
+        #         self.drawStep = 1
+        #     else:
+        #         w = np.array([self.endPt.x() - self.startPt.x(), self.endPt.y() - self.startPt.y()])
+        #         mu = np.dot(w, self.vector) / np.dot(self.vector, self.vector)
+        #         # mu = np.clip(mu, 0, 1)
+        #         dv = mu * self.vector 
+        #         self.curline.setLine(self.startPt.x(), self.startPt.y(), self.startPt.x() + dv[0], self.startPt.y() + dv[1])
+        #         self.curline.setPen(QtCore.Qt.yellow)
+        #         radius = (dv[0]**2 + dv[1] **2) ** 0.5
+        #         center_x, center_y = self.startPt.x() + dv[0], self.startPt.y() + dv[1]
+        #         circle = QGraphicsEllipseItem(center_x - radius, center_y - radius, 2 * radius, 2 * radius)
+        #         pen = QPen(QtCore.Qt.yellow, 1, QtCore.Qt.DotLine)
+        #         circle.setPen(pen)            
+        #         self.scene.addItem(circle)
+        #         self.curline = None
+        #         self.scopline = None
+        #         self.drawStep = 0
     def showData(self, img):
         self.Ny, self.Nx, _ = img.shape
         img_new = np.zeros_like(img)
@@ -200,8 +302,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         qimg = QtGui.QImage(img.data, self.Nx, self.Ny, QtGui.QImage.Format_RGB888)
         pix = QtGui.QPixmap.fromImage(qimg)
         # item = QtWidgets.QGraphicsPixmapItem(pix) 
+        # self.frame.setPixmap(pix)
         self.frame.setPixmap(pix)
-        
         # self.line = QtWidgets.QGraphicsLineItem(self.start_point.x(), self.start_point.y(), self.end_point.x(), self.end_point.y())
                
         # self.scene.addItem(item)
@@ -275,7 +377,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.statusbar.showMessage(str(msg), 5000)
 if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
+    screenGeometry = QtWidgets.QApplication.desktop().screenGeometry()
+    x = screenGeometry.width()
+    y = screenGeometry.height()
     win = MainWindow()
+    win.setGeometry(QtCore.QRect(int(x/10), int(y/10), int(x/1.2), int(y/1.2)))
     win.show()
     sys.exit(app.exec_())
     
